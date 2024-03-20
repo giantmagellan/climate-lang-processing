183acf1f7a8c5fce3e156dc71a057f4ecba55a07
bryan <bryanflores@sandiego.edu>
2024-03-20 00:19:13 -0700

    ngram distributions

--- scripts/eda.py
+++ scripts/eda.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 # DISTRIBUTIONS #
 # ------------- #
 
-def get_top_ngrams(corpus: pd.Series, n_phrases: int=10, ngram_min: int=2, ngram_max: int=2, stopwords: bool=True) -> pd.DataFrame:
+def get_top_n_ngram(corpus: pd.Series, n_phrases: int=10, ngram_min: int=2, ngram_max: int=2, stopwords: bool=True) -> sns.barplot:
     """
     Finds most common n-gram phrases in a given corpus and plot its distribution.
     :param corpus: pd.Series, text column
@@ -33,16 +33,35 @@ def get_top_ngrams(corpus: pd.Series, n_phrases: int=10, ngram_min: int=2, ngram
         words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
         words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
 
+        # Store word frequencies as pd.DataFrame for plotting
+        df = pd.DataFrame(words_freq[:n_phrases], columns=['snippets', 'count'])
+
+        # Remove climate bi-gram phrases
+        climate_grams = ["climate change", "global warming", "climate crisis", "greenhouse gas", "greenhouse gases", "carbon tax"]
+        df = df[~df['snippets'].isin(climate_grams)]
+
     except ValueError:
         print("Invalid input")
     
-    return pd.DataFrame(words_freq[:n_phrases], columns=['snippets', 'count'])
+    return plot_ngram_dist(df)
 
 
-def plot_ngram_dist(df: pd.DataFrame) -> sns.histplot:
+def plot_ngram_dist(df: pd.DataFrame) -> None:
     """ 
     Plot the n-gram distribution.
     :param df: pd.DataFrame, dataframe of climate news snippets.
     :return: sns.histplot
     """
-    return sns.barplot(df, x='snippets', y='count', legend=False)
+    sns.set_style('darkgrid')
+    sns.set_theme(rc={'figure.figsize':(10,4)})
+
+    # Plot bar chart of n-gram frequencies
+    sns.barplot(df, x='snippets', y='count', legend=False)
+    plt.xticks(rotation=45)
+    plt.title("Phrase Frequencies")
+    plt.xlabel("N-Grams")
+    plt.ylabel("Count")
+
+    # Save and show plot
+    plt.savefig('figures/n_gram_frequencies.png')
+    plt.show()
