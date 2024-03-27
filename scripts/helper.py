155d3bd797412afd4d6448a03efd77762d699181
bryan <bryanflores@sandiego.edu>
2024-03-26 20:07:25 -0700

    llm response function

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,4 +1,23 @@
+import os
 import pandas as pd
 
-def get_llm_response(prompt: str) -> str:
-    return None  # Placeholder
+from openai import OpenAI
+
+
+def get_llm_response(prompt: str, model_url: str) -> str:
+    """
+    Get response from ChatGPT.
+    :param prompt: str, instruction prompt for LLM
+    :param model_url: str, model name
+    :return: str, LLM response
+    """
+    
+    if model_url in ["gpt-3.5-turbo-0125", "gpt-4"]:
+        client = OpenAI(api_key=os.environ.get('OPEN_AI_KEY'))
+
+        response = client.chat.completions.create(
+            model = model_url,
+            messages = [{"role": "user", "content": prompt}],
+            temperature = 0.7,
+        )
+        return response.choices[0].message.content 
---
+++ scripts/sentiment_analysis.py
@@ -0,0 +1,8 @@
+import time
+
+from helper import get_llm_response
+
+
+# TODO: Add prompting for sentiment analysis
+def generate_llm_output(payload: dict=None) -> str:
+    return None  # Placeholder
