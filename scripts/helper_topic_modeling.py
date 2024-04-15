29ce72eacc374cb14f2aad938d8b78cf88444395
bryanflores1 <bryan.flores@xselltechnologies.com>
2024-04-14 19:42:24 -0700

    main nb sections: topic modeling

--- scripts/helper_topic_modeling.py
+++ scripts/helper_topic_modeling.py
@@ -1,14 +1,24 @@
 import numpy as np
 from typing import Tuple
 
+from string import punctuation
+from nltk.corpus import stopwords
+from collections import defaultdict, Counter
+from wordcloud import WordCloud
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import NMF, TruncatedSVD, LatentDirichletAllocation
+import pyLDAvis
+import pyLDAvis.lda_model
+import pyLDAvis.gensim_models
 
-# -------------- #
-# TOPIC LABELING #
-# -------------- #
+# --------------------- #
+# LARGE LANGUAGE MODELS #
+# --------------------- #
 
 def assign_categories(topic_labels: np.ndarray, num_clusters: int=5) -> dict:
     """
@@ -64,3 +74,31 @@ def cluster_topic_labels(topic_labels: np.ndarray, num_clusters=5) -> Tuple[np.n
     category_names = [topic_labels[index] for index in closest]
 
     return km.labels_, category_names
+
+
+# --------------------------- #
+# LATENT DIRICHLET ALLOCATION #
+# --------------------------- #
+
+def get_topics_from_lda(corpus: pd.Series, ngram_min: int=2, ngram_max: int=2):
+
+    count_vectorizer = CountVectorizer(stop_words='english', min_df=5, max_df=0.7, 
+                                       ngram_range=(ngram_min, ngram_max))
+    count_vectors = count_vectorizer.fit_transform(corpus)
+
+    lda_model = LatentDirichletAllocation(n_components=5, random_state=314)
+    W_lda_matrix = lda_model.fit_transform(count_vectors)
+    H_lda_matrix = lda_model.components_
+
+    display_topics(lda_model, count_vectorizer.get_feature_names_out())
+
+    return lda_model
+
+
+def display_topics(model, features, no_top_words=5):
+    for topic, words in enumerate(model.components_):
+        total = words.sum()
+        largest = words.argsort()[::-1] # invert sort order
+        print("\nTopic %02d" % topic)
+        for i in range(0, no_top_words):
+            print("  %s (%2.2f)" % (features[largest[i]], abs(words[largest[i]]*100.0/total)))
